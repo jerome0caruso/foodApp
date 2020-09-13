c2e4489a774a6fab4edf1241cf97b11db1ad7b24
jerome0caruso <jerome0caruso@myjavascriptprojects.com>
2020-09-13 12:12:35 -0500

    Updates

---
+++ precache-manifest.37877d56813ae98f385768bc3c22661b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876bc47a8ea4b9b57685276e5270dd1f",
+    "url": "/foodApp/index.html"
+  },
+  {
+    "revision": "a8637334e68a20dae660",
+    "url": "/foodApp/static/css/main.2dbf8695.chunk.css"
+  },
+  {
+    "revision": "649e58c7d40eb78df141",
+    "url": "/foodApp/static/js/2.7a235355.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/foodApp/static/js/2.7a235355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8637334e68a20dae660",
+    "url": "/foodApp/static/js/main.acf78c00.chunk.js"
+  },
+  {
+    "revision": "190b3e5ba85fc7508aa6",
+    "url": "/foodApp/static/js/runtime-main.5af1c0d6.js"
+  },
+  {
+    "revision": "037a92e0aabb69868e9088ac06197fb9",
+    "url": "/foodApp/static/media/testpicfood.037a92e0.jpg"
+  }
+]);
